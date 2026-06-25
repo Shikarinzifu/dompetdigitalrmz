aa35d1b47da4d0993f2939cd99cbe2572421a072
Ramzscuyyy <1123150076@global.ac.id>
2026-06-25 20:49:11 +0700

    refactor: ubah RegisterWithOtpUsecase terima name, email, password alih-alih firebaseToken

--- lib/domain/usecases/auth/register_with_otp_usecase.dart
+++ lib/domain/usecases/auth/register_with_otp_usecase.dart
@@ -5,7 +5,11 @@ class RegisterWithOtpUsecase {
   final AuthRepository _repository;
   RegisterWithOtpUsecase(this._repository);
 
-  Future<({UserEntity user, String token})> call(String firebaseToken) {
-    return _repository.registerWithOtp(firebaseToken);
+  Future<({UserEntity user, String token})> call({
+    required String name,
+    required String email,
+    required String password,
+  }) {
+    return _repository.registerWithOtp(name: name, email: email, password: password);
   }
 }
